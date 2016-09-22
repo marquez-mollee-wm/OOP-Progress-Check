e91993e317923af04f26daa55a51490cf06559cd
Mollee <session1@START-Coding-MBP-14.local>
2016-09-22 15:11:08 -0700

    Mollie Marquez - Progress Check 2

--- class_lib.php
+++ class_lib.php
@@ -33,3 +33,66 @@ class playlist{
         return $this->name;
     }
 }
+
+abstract class song{
+
+    public $title;
+    public $artist;
+    public $album;
+
+    public function describe(){
+
+        return  $this->title.' on '. $this->album . ' by ' . $this->artist ;
+
+    }
+
+    abstract public function number();
+
+}
+
+class song1 extends song{
+
+    public function describe()
+    {
+        return parent::describe();
+    }
+
+    public function number()
+    {
+        return '1. ';
+    }
+
+}
+
+class song2 extends song{
+    public function describe()
+    {
+        return parent::describe();
+    }
+
+    public function number()
+    {
+        return '2. ';
+    }
+}
+
+class song3 extends song{
+    public function describe()
+    {
+        return parent::describe();
+    }
+
+    public function number()
+    {
+        return '3. ';
+    }
+}
+
+class Users{
+    public $user1 = 'Rogelio';
+    public $user2 = 'Marques';
+    public $user3 = 'Breadman';
+    public $user4 = 'Sean';
+    public $user5 = 'Nick';
+
+}
--- index.php
+++ index.php
@@ -1,4 +1,15 @@
-<?php include("class_lib.php"); ?>
+<?php
+require ('class_lib.php');
+
+spl_autoload_register(function($class_name){
+    include $class_name.'.php';
+
+});
+
+$bar = new Bar;
+$foo = new Foo;
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -10,11 +21,36 @@
         $user1->set_name('Mollee');
         $playName = new playlist("Just Scream!");
         $playName->set_name("Just Scream!");
+        $song1 = new song1();
+        $song1->title = 'The Lines';
+        $song1->album = 'Disgusting';
+        $song1->artist = 'Beartooth';
+        $song2 = new song2();
+        $song2->title = 'Beaten in Lips';
+        $song2->album ='Disgusting';
+        $song2->artist='Beartooth';
+        $song3 = new song3();
+        $song3->title = 'Body Bag';
+        $song3->album ='Disgusting';
+        $song3->artist='Beartooth';
 
-        echo "The users first name is " . $user1->get_name(). "<br>";
-        echo $user1->get_name() . " has created a playlist called" . $playName->get_name(). ".";
+        echo "<h1>Welcome " . $user1->get_name(). "</h1><br>";
+        echo $user1->get_name() . " has created a playlist called " . $playName->get_name(). ".<br>";
+        echo $song1 ->number();
+        echo $song1->describe() . '<br>';
+        echo $song2->number();
+        echo $song2->describe() . '<br>';
+        echo $song3->number();
+        echo $song3->describe(). '<br>';
 
-    ?>
+        echo '<h3>Other users on:</h3>';
+
+        $users = new Users();
+
+        foreach($users as $key => $value){
+             print "$value" . '<br>';
+        }
+        ?>
 </head>
 <body>
 
